922927ebd30f28ba7a738f1cb51135f1db7ce1b0
ananjaser1211 <ananjaser@gmail.com>
2019-01-01 13:45:56 +0100

    common: Update RIL packages

---
+++ product/radio.mk
@@ -0,0 +1,12 @@
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.deprecated@1.0 \
+    libsecril-client \
+    libsecril-client-sap \
+    rild \
+    libreference-ril \
+    libril \
+    modemloader \
+    libxml2 \
+    libprotobuf-cpp-full
