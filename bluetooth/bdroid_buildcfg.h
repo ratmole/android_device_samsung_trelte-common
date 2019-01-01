1b1440e1f28318c549df4fd5562c636e22657d68
ripee <ripee.dev@gmail.com>
2019-01-01 14:11:03 +0100

    common: Rename bluetooth device to Galaxy Note 4

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Galaxy Tab S2"
+#define BTM_DEF_LOCAL_NAME "Galaxy Note 4"
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
