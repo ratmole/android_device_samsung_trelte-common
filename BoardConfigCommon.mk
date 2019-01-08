a52874578a10c474f86963ec676019ef0ce3e1ff
AnanJaser <ananjaser@gmail.com>
2019-01-08 05:22:09 +0400

    trelte-common: remove bauth shim

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -201,7 +201,6 @@ TARGET_NO_SENSOR_PERMISSION_CHECK := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib/libbauthserver.so|libbauthtzcommon_shim.so \
     /system/vendor/lib/libexynoscamera.so|libexynoscamera_shim.so \
     /system/vendor/bin/gpsd|libshim_gpsd.so \
     /system/lib/libsec-ril.so|libsec-ril_shim.so
