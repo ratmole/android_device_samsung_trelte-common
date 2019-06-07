55a1abd002314191ed4c2b2e487d0d8b5587f7b1
AnanJaser <ananjaser@gmail.com>
2019-06-07 19:25:45 +0400

    trelte-common: import and refactor ZeroDoze
    Co-Authored-By: Lukas Berger <mail@lukasberger.at>

--- device-common.mk
+++ device-common.mk
@@ -100,6 +100,10 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGES += \
 #   AdvancedDisplay
 
+# Doze
+PRODUCT_PACKAGES += \
+	TrelteDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
