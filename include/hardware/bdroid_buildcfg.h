477b29b925f1e4c4439af46e2286e73fadcf06c7
AnanJaser <ananjaser@gmail.com>
2019-08-26 01:23:20 +0400

    trelte-common: reorganize bluetooth

--- include/hardware/bdroid_buildcfg.h
+++ include/hardware/bdroid_buildcfg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The CyanogenMod Project <http://www.cyanogenmod.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +18,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "Galaxy Note 4"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note 4"
 
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+#define BLE_VND_INCLUDED         TRUE
+#define BTIF_HF_WBS_PREFERRED    TRUE    /* Use WBS */
+#define BTM_WBS_INCLUDED         TRUE    /* Enable WBS */
 
 #endif
