68be125920c0d68d447906c4173cb7728aeecc71
ananjaser1211 <ananjaser@gmail.com>
2019-09-12 09:32:15 +0400

    trelte-common: add basic NFC setup
    based of TeamNexus

---
+++ product/nfc.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := device/samsung/trelte-common
+
+# Configs
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:system/vendor/etc/libnfc-nci.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/vendor/etc/libnfc-sec.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/vendor/etc/libnfc-sec-hal.conf
+
+## Packages
+PRODUCT_PACKAGES += \
+	libnfc-nci \
+	libnfc_nci_jni \
+	NfcNci \
+	Tag \
+	com.android.nfc_extras \
+	android.hardware.nfc@1.0-impl \
+	android.hardware.nfc@1.0-service
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+	frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+	frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.nfc.sec_hal=true \
+	ro.nfc.port="I2C"
