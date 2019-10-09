37102752434a655595fb1b58d0757774761ed498
Kevin F. Haggerty <haggertk@lineageos.org>
2019-10-09 10:19:54 +0400

    trelte-common: Set SDK API level for mediaserver
    * This singularly handles both text relocation (M) and mutex (P)
      linker restrictions
    * mediaserver - Needs text relocations
    
    Change-Id: I97a79fb57aaadf5cf62894489522b2de6d8c2e36

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -228,7 +228,9 @@ TARGET_LD_SHIM_LIBS := \
 
 # Legacy BLOB Support
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
-    /system/vendor/bin/hw/rild=27
+    /system/vendor/bin/hw/rild=27 \
+		/system/bin/mediaserver=22
+
 
 # Network Routing
 TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE := true
