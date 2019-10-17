ce70a421f8fec2a5e596a10ff9ad18086d675493
AnanJaser <ananjaser@gmail.com>
2019-10-18 02:49:04 +0400

    trelte-common: remove obselete media_profiles and update used one
    * inline with stock

--- device-common.mk
+++ device-common.mk
@@ -209,7 +209,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
-    $(COMMON_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # Memory
