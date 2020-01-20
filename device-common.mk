06bad9ba066a51267cce875c0b6a5507eda34dbe
AnanJaser <ananjaser@gmail.com>
2020-01-20 22:23:42 +0400

    trelte-common: Use low-end default video codec
    Co-Authored-By: Dario Trombello <dariotr@outlook.de>

--- device-common.mk
+++ device-common.mk
@@ -205,7 +205,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video_le.xml \
     $(COMMON_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
