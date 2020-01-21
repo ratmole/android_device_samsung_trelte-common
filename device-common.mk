6ba1e54bb604f6beeb9f1c658ccead9cfb68fe7a
AnanJaser <ananjaser@gmail.com>
2020-01-21 23:47:51 +0400

    trelte-common: move priv-app validty permission to SYSTEM
    moving this to vendor breaks it for some reason

--- device-common.mk
+++ device-common.mk
@@ -267,7 +267,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
-    $(COMMON_PATH)/configs/permissions/privapp-permissions-com.validity.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-com.validity.fingerprint.xml \
+    $(COMMON_PATH)/configs/permissions/privapp-permissions-com.validity.fingerprint.xml:system/etc/permissions/privapp-permissions-com.validity.fingerprint.xml \
     $(COMMON_PATH)/configs/permissions/com.samsung.permission.HRM_EXT.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.HRM_EXT.xml \
     $(COMMON_PATH)/configs/permissions/com.samsung.permission.SSENSOR.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.SSENSOR.xml
 
